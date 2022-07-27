f6b114198c8704d842d6c596503250b89ba6cf66
SantoshBhanuse <108928681+SantoshBhanuse@users.noreply.github.com>
2022-07-27 15:49:06 +0530

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,10 @@
+public class Myclass 
+{
+	public static void main(String[] args)
+	{
+		double myDouble=10.46;
+		int myInt = (int) myDouble;
+		System.out.println(myDouble);
+		System.out.println(myInt);
+	}
+}
