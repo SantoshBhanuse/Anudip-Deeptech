4f327e6d94dc1ac9ba10a492ebd4347c00d4a96e
SantoshBhanuse <108928681+SantoshBhanuse@users.noreply.github.com>
2022-07-28 14:26:41 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Arithmetic
+{
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+	      int a = sc.nextInt();
+	      int b = sc.nextInt();
+	      System.out.print("sum: ");
+		System.out.println(a+b);
+		System.out.print("sub: ");
+		System.out.println(a-b);
+		System.out.print("multipication: : ");
+		System.out.println(a*b);
+		System.out.print("division: ");
+		System.out.println(a/b);
+		System.out.print("modulus: ");
+		System.out.println(a%b);
+	}
+}
