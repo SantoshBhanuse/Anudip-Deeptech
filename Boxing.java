faf8ad538ea0d5225e0ef78d2ee4262d89209386
SantoshBhanuse <108928681+SantoshBhanuse@users.noreply.github.com>
2022-07-28 15:27:34 +0530

    Add files via upload

---
+++ Boxing.java
@@ -0,0 +1,10 @@
+class Boxing
+{
+	public static void main(String args[])
+	{
+		int a=50;
+		Integer b2=new Integer(a);
+		Integer b3=20;
+		System.out.println(b2+" "+b3);
+	}
+}
---
+++ Unboxing.java
@@ -0,0 +1,9 @@
+class Unboxing
+{
+	public static void main(String[] args)
+	{
+		Integer i=new Integer(100);
+		int a=i;
+		System.out.println(a);
+	}
+}
