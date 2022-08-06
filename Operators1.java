a2a7ca6a6b9587084a056235bb346b8993b642c0
SantoshBhanuse <108928681+SantoshBhanuse@users.noreply.github.com>
2022-08-06 14:34:35 +0530

    Add files via upload

---
+++ Operators1.java
@@ -0,0 +1,20 @@
+import java.lang.String;
+public class Operators1 
+{
+    public static void main(String[] args)
+    {
+        int a = 10, b = 5;
+        String x = "Thank", y = "Thank";
+        int ar[] = { 1, 2, 3 };
+        int br[] = { 1, 2, 3 };
+        Boolean condition = true;
+        System.out.println("a == b :" + (a == b));
+        System.out.println("a < b :" + (a < b));
+        System.out.println("a <= b :" + (a <= b));
+        System.out.println("a > b :" + (a > b));
+        System.out.println("a >= b :" + (a >= b));
+        System.out.println("a != b :" + (a != b));
+        System.out.println("x == y : " + (ar == br));
+        System.out.println("condition==true :" + (condition == true));
+    }
+}
