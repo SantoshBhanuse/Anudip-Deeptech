d87d37b71a092fe8710dd859cfff665943c28066
SantoshBhanuse <108928681+SantoshBhanuse@users.noreply.github.com>
2022-08-06 14:49:03 +0530

    Add files via upload

---
+++ ObjectsClass.java
@@ -0,0 +1,14 @@
+import java.lang.String;
+public class ObjectsClass
+{
+	int x=6;
+	int y=7;
+	int z=8;
+	public static void main(String[]args)
+	{
+		ObjectsClass myObj = new ObjectsClass();
+		System.out.println(myObj.x);
+		System.out.println(myObj.y);
+		System.out.println(myObj.z);
+	}
+}
